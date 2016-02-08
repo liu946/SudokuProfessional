2e1d6c769c4e8213b7f29b320cd8946743f18074
liu946 <liuyang570926881@gmail.com>
2016-02-08 21:34:12 +0800

    my information

--- js/menu.js
+++ js/menu.js
@@ -11,14 +11,14 @@ $(function () {
 
   // Add some items with label
   menu.append(new nw.MenuItem({
-    label: 'Item A',
+    label: '作者信息',
     click: function () {
-      alert('You have clicked at "Item A"');
+      alert('Michael Castor (Liu Yang) - HIT. \nAll right reserved. 2016\nhttp://liu946.github.io/');
     }
   }));
-  menu.append(new nw.MenuItem({label: 'Item B'}));
-  menu.append(new nw.MenuItem({type: 'separator'}));
-  menu.append(new nw.MenuItem({label: 'Item C'}));
+  //menu.append(new nw.MenuItem({label: 'Item B'}));
+  //menu.append(new nw.MenuItem({type: 'separator'}));
+  //menu.append(new nw.MenuItem({label: 'Item C'}));
 
   // Hooks the "contextmenu" event
   document.body.addEventListener('contextmenu', function (ev) {
